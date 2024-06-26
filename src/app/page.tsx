1329501aee25492a12aa74ddcadd7be355ce9218
Leo Heng <leoheng905@gmail.com>
2024-06-26 23:20:20 +1000

    Added brand colours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
+import TestComponent from '@/components/TestComponent/TestComponent';
 
 export default function Home() {
   return (
     <main className="">
       <h1 className="font-display text-4xl">VICHACK</h1>
-
-      <p>Poppins</p>
+      <TestComponent></TestComponent>
+      <p className="text-purple-light">Poppins</p>
     </main>
   );
 }
