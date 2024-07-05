cd63fd5e0a0593bf652703aeb13d432a1716aae9
Michael Ren <michaelren0129@gmail.com>
2024-07-05 19:05:21 +1000

    Change absolute to static positioning

--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -17,9 +17,8 @@ import LightningImage from '@/assets/hero-shapes/lightning.svg'
 import SawBladeImage from '@/assets/hero-shapes/sawBlade.svg'
 import TrianglesImage from '@/assets/hero-shapes/triangles.svg'
 
-const GRID_CENTER_OFFSET = 0.947 // Percentage offset to align text to grid center
-const BUTTON_MARGIN = 40 // Margin between Vichack text and sign up button
-const INFO_MARGIN = 40 // Margin between Vichack text and sign up button
+// The center line is at 47.1% of the grid's height
+const GRID_CENTER_OFFSET = 0.471
 
 function Navbar() {
   return (
@@ -27,7 +26,7 @@ function Navbar() {
       <div className="space-y-1">
         <p className="">11/06 - 15/06</p>
         <h3 className="text-xl sm:text-2xl font-display">VICHACK</h3>
-        <Image className="max-w-14 xs:max-w-none" src={TrianglesImage} alt="Shape"/>
+        <Image className="max-w-12 xs:max-w-none" src={TrianglesImage} alt="Shape"/>
       </div>
       <div className="flex gap-1 flex-col text-right font-bold sticky top-0">
         <Link href="#">HOME</Link>
@@ -39,60 +38,73 @@ function Navbar() {
 }
 
 export default function HeroSection() {
-  const [gridHeight, setGridHeight] = useState(0)
-  const [textHeight, setTextHeight] = useState(0)
+  // const [gridHeight, setGridHeight] = useState(0)
+  const [logoY, setLogoY] = useState(0)
   const gridRef: React.RefObject<SVGSVGElement> | null = useRef(null)
-  const textRef: React.RefObject<HTMLImageElement> | null = useRef(null)
+  const logoRef: React.RefObject<HTMLImageElement> | null = useRef(null)
 
-  function getHeights() {
-    if (!gridRef?.current || !textRef?.current)
+  function setLogoPosition() {
+    if (!gridRef?.current || !logoRef?.current)
       return 
 
+    // Calculate the mid point of the grid
     const gridRect = gridRef.current.getBoundingClientRect()
-    setGridHeight(gridRect.top * 2 + gridRect.height + window.scrollY * 2)
+    const midPointY = (gridRect.y + gridRect.height * GRID_CENTER_OFFSET + window.scrollY)
 
-    const textRect = textRef.current.getBoundingClientRect()
-    setTextHeight(textRect.height)
+    // Calculate the logo's inner height
+    const logoElem = logoRef.current
+    const logoRect = logoElem.getBoundingClientRect()
+    const { paddingTop, paddingBottom } = getComputedStyle(logoElem)
+    const logoInnerHeight = logoRect.height - parseFloat(paddingTop) - parseFloat(paddingBottom)
+
+    // Set the logo's y position
+    setLogoY(midPointY - logoInnerHeight / 2)
   }
-
-  function getTextTop() {
-    return (gridHeight * GRID_CENTER_OFFSET - textHeight) / 2
-  }
-
-  function getButtonTop() {
-    return (gridHeight * GRID_CENTER_OFFSET + textHeight) / 2 + BUTTON_MARGIN
-  }
-
-  // function getInfoTop() {
-  //   return (gridHeight * GRID_CENTER_OFFSET + textHeight) / 2 + INFO_MARGIN
-  // }
 
   useEffect(() => {
-    getHeights()
-    window.addEventListener('resize', getHeights)
-    return () => window.removeEventListener('resize', getHeights)
-  }, [gridRef])
+    setLogoPosition()
+    window.addEventListener('resize', setLogoPosition)
+  }, [gridRef, logoRef])
 
   return (
-    <div className="relative min-h-[500px] sm:min-h-[800px] md:min-h-[720px] h-svh transition-opacity duration-500" style={{
+    <div className="transition-opacity duration-500" style={{
       // Hide the hero section until images are loaded and absolute positions are calculate
-      'opacity': gridHeight ? 1 : 0
+      'opacity': logoY ? 1 : 0
     }}>
       <Navbar />
 
-        <PerspectiveGrid ref={gridRef} className="absolute left-1/2 -translate-x-1/2 w-full h-full md:h-[110%] sm:top-0 translate-y-[-50px] sm:translate-y-0 "/>
-        
-        <Image ref={textRef} src={LogoImage} alt="VicHack Logo" className="absolute w-[300px] xs:w-[360px] md:w-[480px] left-1/2 -translate-x-1/2 z-[2]" style={{
-          top: getTextTop() + 'px',
+      {/* Grid image, the position of all other static elements are relative to the center of the grid */}
+      <PerspectiveGrid ref={gridRef} className="absolute w-full min-h-[640px] h-[125%] top-[-20%] sm:top-1/2 sm:-translate-y-1/2 md:top-0 md:-translate-y-0 left-1/2 -translate-x-1/2"/>
+      
+      <div className="relative flex flex-col w-full min-h-svh gap-10 z-2">
+        {/* The padding top of this element is responsible for pushing all other static elements down */}
+        <Image ref={logoRef} src={LogoImage} alt="VicHack Logo" className="w-[300px] xs:w-[360px] md:w-[480px] mx-auto" style={{
+          paddingTop: logoY + 'px',
         }}/>
 
-        <Button href="#" className="absolute left-1/2 -translate-x-1/2 z-[2]" style={{
-          top: getButtonTop() + 'px'
-        }}>
-          SIGNUP
-        </Button>
+        <Button href="#" className="mx-auto">SIGNUP</Button>
 
-      <ParallaxAnimator className="left-[15vw] top-[max(100px,20svh)] md:left-[30vw] md:top-[max(185px,25svh)]" speed={100}>
+        <div className="flex gap-10 justify-between w-full mt-auto p-5 xs:p-10 flex-col md:flex-row">
+          <div className="space-y-2 my-auto w-fit">
+            <Image src={DiamondsImage} alt="Shape"/>
+            <p className="font-display max-w-60 md:max-w-none text-xl sm:text-2xl">SHAPE THE FUTURE</p>
+          </div>
+
+          <div className="flex gap-10 min-w-none md:min-w-max">
+            <Image className="max-w-16 sm:max-w-none hidden xs:flex" src={GridShapesImage} alt="Shape"/>
+            <div className="space-y-1">
+              <p className="text-sm font-bold">HACKMELBOURNE</p>
+              <p className="text-sm font-bold">MONASH ASSOC. OF CODING</p>
+              <p className="text-sm font-bold">RMIT INFORMATION SECURITY COLLECTIVE</p>
+              <p className="text-sm font-bold">MONASH DEEPNEURON</p>
+              <p className="text-sm font-bold">DATA SCIENCE STUDENTS SOCIETY</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      {/* <ParallaxAnimator className="left-[15vw] top-[max(100px,20svh)] md:left-[30vw] md:top-[max(185px,25svh)]" speed={100}>
         <Image src={SawBladeImage} alt="Shape"/>
       </ParallaxAnimator>
 
@@ -110,25 +122,9 @@ export default function HeroSection() {
 
       <ParallaxAnimator className="left-[70vw] top-[max(100px,15svh)] md:left-[20vw] md:top-[max(481px,65svh)]" speed={1500}>
         <Image src={FlowerThinImage} alt="Shape"/>
-      </ParallaxAnimator>
+      </ParallaxAnimator> */}
 
-      <div className="flex gap-10 flex-col md:flex-row justify-between w-full absolute bottom-0 p-5 xs:p-10">
-        <div className="space-y-2 my-auto w-fit">
-          <Image src={DiamondsImage} alt="Shape"/>
-          <p className="font-display max-w-60 md:max-w-none text-xl sm:text-2xl">SHAPE THE FUTURE</p>
-        </div>
-
-        <div className="flex gap-10 sm:min-w-max">
-          <Image className="max-w-16 sm:max-w-none xs:block hidden" src={GridShapesImage} alt="Shape"/>
-          <div className="space-y-1">
-            <p className="text-sm font-bold">HACKMELBOURNE</p>
-            <p className="text-sm font-bold">MONASH ASSOC. OF CODING</p>
-            <p className="text-sm font-bold">RMIT INFORMATION SECURITY COLLECTIVE</p>
-            <p className="text-sm font-bold">MONASH DEEPNEURON</p>
-            <p className="text-sm font-bold">DATA SCIENCE STUDENTS SOCIETY</p>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 }
