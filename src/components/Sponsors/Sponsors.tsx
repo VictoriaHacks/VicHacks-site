bcafac2649ab46a077abd6c801a2568a6ee76aa1
Leo Heng <leoheng905@gmail.com>
2024-07-20 17:08:03 +1000

    updated rea sponsor

--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -1,32 +1,40 @@
-import React from "react";
+import React from 'react';
 
-import Image from "next/image";
-import center from "@/assets/shapes/center.svg";
+import Image from 'next/image';
+import center from '@/assets/shapes/center.svg';
 
-import GoldSponsorBlock, { goldSponsorBlock } from "./GoldSponsorBlock";
-import SilverSponsorBlock, { silverSponsorBlock } from "./SilverSponsorBlock";
+import GoldSponsorBlock, { goldSponsorBlock } from './GoldSponsorBlock';
+import SilverSponsorBlock, { silverSponsorBlock } from './SilverSponsorBlock';
 
 // Sponsors
-import citadelLogo from "@/assets/sponsors/CitSec-Logo.webp";
-import janeStLogo from "@/assets/sponsors/JaneStreet.png";
+import citadelLogo from '@/assets/sponsors/CitSec-Logo.webp';
+import janeStLogo from '@/assets/sponsors/JaneStreet.png';
+import reaLogo from '@/assets/sponsors/reaLogo.png';
 
 export default function Sponsors() {
   const goldSponsors: goldSponsorBlock[] = [
     {
-      title: "CITADEL Securities",
+      title: 'CITADEL Securities',
       img: citadelLogo,
-      alt: "Citadel Logo",
-      desc: "Our ambition is to be the most successful investment firm of all time. We strive to identify the highest and best uses of capital to generate superior long-term returns for the world’s preeminent public and private institutions.",
-      link: "https://www.citadelsecurities.com/",
+      alt: 'Citadel Logo',
+      desc: 'Our ambition is to be the most successful investment firm of all time. We strive to identify the highest and best uses of capital to generate superior long-term returns for the world’s preeminent public and private institutions.',
+      link: 'https://www.citadelsecurities.com/',
+    },
+    {
+      title: 'REA Group',
+      img: reaLogo,
+      alt: 'REA Group Logo',
+      desc: 'REA Group is not your average digital business. With almost 3,000 people across three continents, we’re working to change the way the world experiences property.',
+      link: 'https://www.rea-group.com/',
     },
   ];
 
   const silverSponsors: silverSponsorBlock[] = [
     {
-      title: "Jane Street",
+      title: 'Jane Street',
       img: janeStLogo,
-      alt: "Jane Street Logo",
-      desc: "Solving the puzzle of global markets, Jane Street is a research-driven trading firm where curious people work together on deep problems",
+      alt: 'Jane Street Logo',
+      desc: 'Solving the puzzle of global markets, Jane Street is a research-driven trading firm where curious people work together on deep problems',
     },
   ];
 
@@ -40,12 +48,12 @@ export default function Sponsors() {
           mission to shape the future!
         </p>
       </div>
-      <div className="flex flex-col w-full mt-16">
+      <div className="flex flex-col w-full mt-16 gap-8">
         {goldSponsors.map((value, index) => {
           return <GoldSponsorBlock {...value} key={index} />;
         })}
       </div>
-      <div className="flex flex-auto justify-center gap-8 mt-16 px-8 sm:px-0">
+      <div className="flex flex-auto justify-center gap-8 mt-24 px-8 sm:px-0">
         {silverSponsors.map((value, index) => {
           return <SilverSponsorBlock {...value} key={index} />;
         })}
